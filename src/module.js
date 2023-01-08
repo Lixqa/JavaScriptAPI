dd25e17252a15ea4a39c359b568f5edd5ef4bbc7
Lixqa <filippodstawski21@gmail.com>
2023-01-08 19:16:15 +0100

    overwritten readme and added lixqaApi check

--- src/module.js
+++ src/module.js
@@ -7,26 +7,28 @@ async function sendApi(parameters) {
     let result;
 
     await axios({
-      method: parameters.method || ApiRequestMethod.GET,
       url: parameters.url,
+      method: parameters.method || ApiRequestMethod.GET,
       headers: parameters.headers,
       data: JSON.stringify(parameters.body)
     })
     .then(response => {
       result = response.data;
 
-      logger.logByOptions({
-        message: "Message: " + chalk.underline.bold(result.message) +
-        "\nName: " + chalk.underline.bold(result.endpoint.name) +
-        "\nRequest duration: " + chalk.underline.bold((new Date().getTime() - responseStartDate)) + "ms" +
-        "\nIntern handle duration: " + chalk.underline.bold(result.duration) + "ms" +
-        "\nMethod: " + chalk.underline.bold(result.method),
-        name: "API",
-        outerSpace: true,
-        baseColor: (result.error) ? chalk.red : chalk.green,
-        borderChar: "-",
-        borderCharLength: 20
-      });
+      if(parameters.url.contains("api.lixqa.de")) {
+        logger.logByOptions({ //only compatible with lixqa api
+          message: "Message: " + chalk.underline.bold(result.message) +
+          "\nName: " + chalk.underline.bold(result.endpoint.name) +
+          "\nRequest duration: " + chalk.underline.bold((new Date().getTime() - responseStartDate)) + "ms" +
+          "\nIntern handle duration: " + chalk.underline.bold(result.duration) + "ms" +
+          "\nMethod: " + chalk.underline.bold(result.method),
+          name: "API",
+          outerSpace: true,
+          baseColor: (result.error) ? chalk.red : chalk.green,
+          borderChar: "-",
+          borderCharLength: 20
+        });
+      }
     })
     .catch(error => {
       console.log(error);
