01e37d370cff841c9cde5aeb450c7995f39bfd36
Lixqa <filippodstawski21@gmail.com>
2023-05-18 15:31:45 +0200

    added tooltip descriptions

--- src/module.js
+++ src/module.js
@@ -2,6 +2,20 @@ const axios = require("axios");
 const logger = require("lixqajslogger");
 const chalk = require("chalk");
 
+
+/**
+ * Log something into console.
+ * @param {Object} parameters - Setup the request
+ * @param {String} parameters.url - Set the target URL
+ * @param {String} parameters.method - Request method
+ * @param {Object} parameters.headers - Send headers to target (key-value)
+ * @param {Object} parameters.body - Send body to target (object)
+ * 
+ * @param {Object} options - Options for the request
+ * @param {Boolean} options.showOutput - Send log if request was successful (Default: true)
+ * @param {Boolean} options.showError - Send error log if request failed (Default: true)
+ * @param {Boolean} options.showDetailedError - Send Axios error object if request failed (Default: false)
+ */
 async function sendApi(parameters, options = {}) {
     let url = parameters.url;
     let method = parameters.method || ApiRequestMethod.GET;
@@ -36,6 +50,25 @@ async function sendApi(parameters, options = {}) {
     .then(response => {
       result = response.data;
       result.reqDuration = (new Date().getTime() - responseStartDate);
+
+      if(parameters.url.includes("api.lixqa.de")) { //only show output if error occures in lixqa api (add option for it)
+        if(result.error) {
+          logger.logByOptions({ //only compatible with lixqa api
+            message: "Message: " + chalk.underline.bold(result?.message) +
+            "\nName: " + chalk.underline.bold(result?.endpoint?.name) +
+            "\nRequest duration: " + chalk.underline.bold((new Date().getTime() - responseStartDate)) + "ms" +
+            "\nIntern handle duration: " + chalk.underline.bold(result?.duration) + "ms" +
+            "\nMethod: " + chalk.underline.bold(result?.method) +
+            "\nHeaders: " + chalk.underline.bold(Object.entries(headers).length) +
+            "\nBody: " + bodyStr,
+            name: "API",
+            outerSpace: true,
+            baseColor: (result.error) ? chalk.red : chalk.green,
+            borderChar: "-",
+            borderCharLength: -1
+          });
+        }
+      }
 
       if(showOutput) {
         if(parameters.url.includes("api.lixqa.de")) {
